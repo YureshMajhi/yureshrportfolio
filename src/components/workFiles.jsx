5991cca20ba7e8ad2b82a9d796622462fb2addbd
Yuresh Majhi <yureshmee@gmail.com>
2024-01-25 16:50:42 +0545

    added project ecommerce-app

--- src/components/workFiles.jsx
+++ src/components/workFiles.jsx
@@ -1,10 +1,5 @@
 // work images
-import form from "../assets/Form.png";
-import youtube from "../assets/Youtube.png";
-import card from "../assets/card.png";
-import todoList from "../assets/todo-list.png";
-import game from "../assets/RockPaperScissor.png";
-import library from "../assets/library.png";
+import eapp from "../assets/eapp1.png";
 
 // skills images
 import javascript from "../assets/javascript.png";
@@ -17,46 +12,10 @@ import mongodb from "../assets/mongodb.png";
 export const workFiles = () => {
   const workArray = [
     {
-      title: "Form",
-      img: form,
-      demo: "https://yureshmajhi.github.io/HTML-CSS-Form/code.html",
-      code: "https://github.com/YureshMajhi/HTML-CSS-Form",
-    },
-    {
-      title: "Youtube",
-      img: youtube,
-      demo: "https://yureshmajhi.github.io/File/test.html",
-      code: "https://github.com/YureshMajhi/File",
-    },
-    {
-      title: "Card",
-      img: card,
-      demo: "https://yureshmajhi.github.io/Invisible-Card/invisiblecard.html",
-      code: "https://github.com/YureshMajhi/Invisible-Card",
-    },
-    {
-      title: "Todo List",
-      img: todoList,
-      demo: "https://yureshmajhi.github.io/todoList/dist/index.html",
-      code: "https://github.com/YureshMajhi/todoList",
-    },
-    {
-      title: "Rock Paper Scissor",
-      img: game,
-      demo: "https://yureshmajhi.github.io/Rock-Paper-Scissors/code.html",
-      code: "https://github.com/YureshMajhi/Rock-Paper-Scissors",
-    },
-    {
-      title: "Library",
-      img: library,
-      demo: "https://yureshmajhi.github.io/odin-project-library",
-      code: "https://github.com/YureshMajhi/odin-project-library",
-    },
-    {
-      title: "Library",
-      img: library,
-      demo: "https://yureshmajhi.github.io/odin-project-library",
-      code: "https://github.com/YureshMajhi/odin-project-library",
+      title: "Ecommerce App",
+      img: eapp,
+      demo: "https://yurush-store.netlify.app/",
+      code: "https://github.com/YureshMajhi/E-commerce-",
     },
   ];
 
